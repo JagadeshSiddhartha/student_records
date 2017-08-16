57f82a55b40d0ff5dcfe3827387f619402eb6f66
JagadeshSiddhartha <jagadesh.siddhartha@gmail.com>
2017-08-16 10:56:30 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Add Student</title>
+</head>
+<body>
+<form action="http://localhost/student/added.php" method="GET">
+
+</br>
+
+
+          <div class = "form-group">
+              <input type="text" class="form-control" name = "name" placeholder="Full Name">  </div>
+
+         <div class = "form-group">
+              <input type="email" class="form-control" name = "email" placeholder="Email">
+          </div>
+
+           <div class = "form-group">
+             <input type="number" class="form-control" name = "phone" placeholder="Phone number"></div>
+
+
+
+
+
+<table class="table table-bordered">
+
+<div class="modal-footer">
+
+          <input type="submit" class="btn btn-primary btn-lg" name="submit" value="Send" />
+          <button type="button" class="btn btn-default btn-lg" data-dismiss="modal">Close</button>
+</div>
+<p>
+</p>
+</table>
+</form>
+</body>
+</html>
---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+//$conn = @mysqli_connect('localhost','pz_dev','Stp1973R','student_sid') 
+$conn = @mysqli_connect('localhost','root','','student_sid') 
+OR die('cannot connect to mysql  '.
+mysqli_connect_error());
+?>
+
