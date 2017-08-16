edb81bb2fee546bab5d982a4b974b139d7a82052
JagadeshSiddhartha <jagadesh.siddhartha@gmail.com>
2017-08-16 14:26:24 +0530

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$conn = @mysqli_connect('localhost','pz_dev','Stp1973R','student_sid') 
-$conn = @mysqli_connect('localhost','root','','student_sid') 
-OR die('cannot connect to mysql  '.
-mysqli_connect_error());
-?>
-
